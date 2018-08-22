d4628ef95d52b7d72dcec183b4865a2b4ca03588
Gonzalo HQ063 <hq063gon@gmail.com>
2018-08-22 10:35:18 -0300

    Delete config.php

--- lib/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BOT_TOKEN', '422299672:AAEShHj0KhMOZ86bIn6rVuhDfsZp0QvN1vg');
-define('FILE_URL', 'https://api.telegram.org/file/bot'.BOT_TOKEN.'/');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-define('BOT_USERNAME', 'playbytelegrambot');
-define('BOT_URL', 'https://telegram.me/'.BOT_USERNAME);
-
-?>
